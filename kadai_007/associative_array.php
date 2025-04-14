1af05b90a82aded0801631bf150155b568668dde
rika183351 <oer3regit1mahc2@gmail.com>
2025-04-14 12:36:56 +0900

    Add files via upload

---
+++ kadai_007/associative_array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+    <?php
+    $item = ['name' => 'onion','price' => 200,'weight' => 160];
+
+    print_r($item);
+
+   ?>
+</p>
+</body>
+
+</html>
